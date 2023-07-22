319033fddfaebeb507d69babe40c1314021a8889
Samuel <samuelou510@gmail.com>
2023-07-22 18:35:46 -0400

    done, printer took longer than others

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badgeList = []
+    for name in names:
+        badgeList.append(badge_maker(name))
+    return badgeList
 
 def assign_rooms(names):
-    return None
+    roomNum = 1
+    newList = []
+    while roomNum <= 8:
+        for name in names:
+            newList.append(f"Hello, {name}! You'll be assigned to room {roomNum}!")
+            roomNum += 1
+    return newList
 
 def printer(names):
-    return None
+    # for name in names:
+    for element in batch_badge_creator(names):
+        print(element)
+        
+    for element in assign_rooms(names):
+        print (element)
+
+    # return None
